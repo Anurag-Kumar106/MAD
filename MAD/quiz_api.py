ccd9156f4ae2f499f605b20aa8e522128c1d53c1
Anurag-Kumar106 <anurag.kumar.23cse@bmu.edu.in>
2025-05-04 20:05:03 +0530

    Update quiz_api.py

--- MAD/quiz_api.py
+++ MAD/quiz_api.py
@@ -8,7 +8,7 @@ app = FastAPI()
 UPLOAD_FOLDER = "uploads"
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)  
 
-quiz_generator = Gemini_Quiz_Generator(api_key="")
+quiz_generator = Gemini_Quiz_Generator(api_key="AIzaSyAwoGkI2c3NEKWxNpJvczInFlava4Wcr2k")
 
 @app.post("/upload/")
 async def upload_pdf(file: UploadFile = File(...)):
